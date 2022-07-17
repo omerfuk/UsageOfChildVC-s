7f7b329b93a008f4a5bf6308f8fff97ae4278270
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-17 22:07:24 +0300

    Initial Commit

---
+++ UsageOfChildVC's/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsageOfChildVC's
+//
+//  Created by Ömer Faruk Kılıçaslan on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
